65c0c7a7f278567752b265843884b2342a3e3c36
Jonathan Tellechea <tellecheajonathan@gmail.com>
2026-08-13 18:48:43 -0700

    chore: updated for colab run

--- src/config.py
+++ src/config.py
@@ -15,9 +15,9 @@ class GPTConfig:
     
     # Architecture Structure
     vocab_size: int = 1973
-    n_layer: int = 12
-    n_head: int = 6
-    n_embd: int = 384
+    n_layer: int = 6
+    n_head: int = 4
+    n_embd: int = 256
     dropout: float = 0.2
 
     def save(self, path: str) -> None:
